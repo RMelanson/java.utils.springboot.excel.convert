0d64b71969d52fc30dc990a438bb04e73077e69e
RobinMelanson <RobinMelanson@142.107.36.236>
2019-03-15 16:12:34 -0400

    WIP

---
+++ complete/src/main/java/utils/conversion/excel_to_json/custom/XlsToJason.java
@@ -0,0 +1,90 @@
+package utils.conversion.excel_to_json.custom;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import utils.json.JacksonMarshaller;
+
+public class XlsToJason {
+	static String EOLN = "\n";
+	static String defaultDirectory = "C:\\Dev\\microservices\\data\\";
+	static String defaultFileName = "Default";
+	static String defaultInputType = ".xls";
+	static String jsonType = ".json";
+	static String defaultBuildMapName = "jsonRequestALL";
+	static String inputFile = defaultDirectory + defaultFileName + defaultInputType;
+	static String outputFile = defaultDirectory + defaultFileName + jsonType;
+	static String buildMap = defaultDirectory + defaultBuildMapName + jsonType;
+
+	public static void main(String[] args) throws IOException {
+		processArgs(args);
+		processBuildFile(buildMap);
+		String jsonString = Parse.xlsToJSON(inputFile);
+		Parse.writeOutput(outputFile, jsonString);
+	}
+
+	private static void processArgs(String[] parms) {
+		int parmsCount = parms.length;
+		if ((parmsCount % 2) != 0 || parmsCount < 2) {
+			usage();
+			System.exit(-1);
+		}
+		for (int idx = 0; idx < parmsCount; idx+=2) {
+			switch (parms[idx]) {
+			case "-i": inputFile = parms[idx+1];
+			break;
+			case "-o":inputFile = parms[idx+1];
+			break;
+			case "-d":inputFile = parms[idx+1];
+			break;
+			default: usage();
+			System.exit(-1);
+			}
+		}
+	}
+
+	private static void usage() {
+		String usage = "XlsToJason <Parameters ( At least one of input or definition File Path Required)>" + EOLN;
+		usage += "Parameters:\n" + EOLN;
+		usage += "-i inputFilePathName (manditory if not defined in definition file)" + EOLN;
+		usage += "-o outputFilePathName (optional)" + EOLN;
+		usage += "-d definitionFilePath Name (optional JSON file defining input, and output files with required fields";
+	}
+
+	private static void processBuildFile(String buildFile) {
+		LinkedHashMap<?, ?> builder = readBuildFile(buildFile);
+
+		String inputFile = (String) builder.get("INPUT_FILE");
+		String outputFile = (String) builder.get("OUTPUT_FILE");
+		LinkedHashMap<?, ?> sheets = (LinkedHashMap<?, ?>) builder.get("SHEETS");
+		Object sheets2 = (LinkedHashMap<?, ?>) builder.get("SHEETS");
+	}
+
+	private static LinkedHashMap<?, ?> readBuildFile(String buildFile) {
+		LinkedHashMap<?, ?> builderMap = null;
+		File bFile = new File(buildFile);
+		int ch;
+		try {
+			StringBuffer strContent = new StringBuffer("");
+			FileInputStream inputBuildFile = new FileInputStream(bFile);
+			while ((ch = inputBuildFile.read()) != -1)
+				strContent.append((char) ch);
+
+			if (JacksonMarshaller.isValidJSON(strContent.toString())) {
+				builderMap = (LinkedHashMap<?, ?>) JacksonMarshaller.jsonStringToClass(strContent.toString(),
+						LinkedHashMap.class);
+				return builderMap;
+			}
+			inputBuildFile.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File " + bFile.getAbsolutePath() + " could not be found on filesystem");
+		} catch (IOException ioe) {
+			System.out.println("Exception while reading the file" + ioe);
+		} finally {
+		}
+		return builderMap;
+	}
+}
---
+++ complete/src/main/java/utils/conversion/excel_to_json/custom/excelToJSONCustom.java
@@ -0,0 +1,213 @@
+package utils.conversion.excel_to_json.custom;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+
+import utils.json.JacksonMarshaller;
+
+class Parse {
+
+	static String xlsToJSON(String inputFile) {
+		String jsonString = null;
+		File iFile = new File(inputFile);
+		LinkedHashMap<String, ArrayList<Object>> xlsFileReqMap = new LinkedHashMap<String, ArrayList<Object>>();
+		HSSFWorkbook workbook = null;
+		try {
+			workbook = new HSSFWorkbook(new FileInputStream(iFile));
+			xlsFileReqMap = getSheetMaps(workbook);
+			buildJsonXlsMap(iFile, xlsFileReqMap);
+			jsonString = JacksonMarshaller.mapJsonString(xlsFileReqMap);
+			System.out.println(jsonString);
+		} catch (FileNotFoundException e) {
+			System.err.println("Exception" + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("Exception" + e.getMessage());
+		} finally {
+			try {
+				workbook.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return jsonString;
+	}
+
+	static void writeOutput(String outputFile, String jsonString) {
+		try {
+			File oFile = new File(outputFile);
+			FileOutputStream fos = new FileOutputStream(oFile);
+			fos.write(jsonString.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("Exception" + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("Exception" + e.getMessage());
+		} finally {
+		}
+	}
+
+	private static void buildJsonXlsMap(File iFile, LinkedHashMap<String, ArrayList<Object>> xlsFileReqMap) {
+		HSSFWorkbook workbook = null;
+		try {
+			workbook = new HSSFWorkbook(new FileInputStream(iFile));
+
+			for (Entry<String, ArrayList<Object>> sheet : xlsFileReqMap.entrySet()) {
+				getSheetArr(workbook, sheet);
+			}
+
+		} catch (FileNotFoundException e) {
+			System.err.println("Exception" + e.getMessage());
+		} catch (IOException e) {
+			System.err.println("Exception" + e.getMessage());
+		} finally {
+			try {
+				workbook.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void getSheetArr(HSSFWorkbook workbook, Entry<String, ArrayList<Object>> sheetEntry) {
+		
+		String sheetName = sheetEntry.getKey();
+		System.out.println("Sheet Name = " + sheetName);
+		HSSFSheet sheet = workbook.getSheet(sheetName);
+		
+		int rowNum = sheet.getFirstRowNum();
+		Row row = sheet.getRow(rowNum);
+		ArrayList<Object> headerKeys = (ArrayList<Object>) sheetEntry.getValue().clone();
+		ArrayList<Object> sheetArr = getSheetArr(sheet,headerKeys);
+		sheetEntry.setValue(sheetArr);
+	}
+	
+	private static ArrayList<Object> getSheetArr(HSSFSheet sheet, ArrayList<Object> headerKeys) {
+		ArrayList<Object> sheetArr = new ArrayList<Object>();
+		int rowNum = sheet.getFirstRowNum();
+		Row row = sheet.getRow(rowNum);
+		while (++rowNum <= sheet.getLastRowNum()) {
+			row = sheet.getRow(rowNum);
+			LinkedHashMap<Object, Object> dataRowMap = getDataRowMap(headerKeys, row);
+			sheetArr.add(rowNum - 1, dataRowMap);
+		}
+		return sheetArr;
+	}
+
+	private static LinkedHashMap<String, ArrayList<Object>> getSheetMaps(HSSFWorkbook workbook) {
+		LinkedHashMap<String, ArrayList<Object>> sheetMaps = new LinkedHashMap<String, ArrayList<Object>>();
+		int numSheets = workbook.getNumberOfSheets();
+
+		for (int cnt = 0; cnt < numSheets; cnt++) {
+			HSSFSheet sheet = workbook.getSheetAt(cnt);
+			String sheetName = sheet.getSheetName();
+			System.out.println("Sheet Name = " + sheetName);
+			ArrayList<Object> sheetArr = getHeaderKeyArr(sheet);
+			sheetMaps.put(sheetName, sheetArr);
+		}
+		return sheetMaps;
+	}
+
+	private static ArrayList<Object> getHeaderKeyArr(HSSFSheet sheet) {
+		ArrayList<Object> headerArr = new ArrayList<Object>();
+		int rowNum = sheet.getFirstRowNum();
+		Row row = sheet.getRow(rowNum);
+		row.getRowNum();
+		for (int cellNum = row.getFirstCellNum(); cellNum < row.getLastCellNum(); cellNum++) {
+			Cell cell = row.getCell(cellNum);
+			Object cellObjVal = null;
+			try {
+				switch (cell.getCellType()) {
+
+				case BOOLEAN:
+					cellObjVal = cell.getBooleanCellValue();
+					break;
+
+				case NUMERIC:
+					if (DateUtil.isCellDateFormatted(cell)) {
+						SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+						cellObjVal = dateFormat.format(cell.getDateCellValue());
+					} else {
+						cellObjVal = (Double) cell.getNumericCellValue();
+						Double dblVal = (Double) cellObjVal;
+						cellObjVal = dblVal.longValue();
+					}
+					break;
+
+				case STRING:
+					cellObjVal = cell.getRichStringCellValue().getString();
+					break;
+
+				case BLANK:
+					cellObjVal = "NULL";
+					break;
+				default:
+					break;
+				}
+				headerArr.add(cellNum, cellObjVal);
+			} catch (NullPointerException e) {
+				// do something clever with the exception
+//				System.out.println("nullException" + e.getMessage());
+			}
+		}
+		return headerArr;
+	}
+
+	private static LinkedHashMap<Object, Object> getDataRowMap(ArrayList<Object> headerKeys, Row row) {
+		LinkedHashMap<Object, Object> dataMap = new LinkedHashMap<Object, Object>();
+		row.getRowNum();
+		for (int cellNum = row.getFirstCellNum(); cellNum < row.getLastCellNum(); cellNum++) {
+			Cell cell = row.getCell(cellNum);
+			Object cellObjVal = "null";
+			if (cell != null)
+				try {
+					switch (cell.getCellType()) {
+
+					case BOOLEAN:
+						cellObjVal = cell.getBooleanCellValue();
+						break;
+
+					case NUMERIC:
+						if (DateUtil.isCellDateFormatted(cell)) {
+							SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+							cellObjVal = dateFormat.format(cell.getDateCellValue());
+						} else {
+							cellObjVal = (Double) cell.getNumericCellValue();
+							Double dblVal = (Double) cellObjVal;
+							cellObjVal = dblVal.longValue();
+						}
+						break;
+
+					case STRING:
+						cellObjVal = cell.getRichStringCellValue().getString();
+						break;
+
+					case BLANK:
+						cellObjVal = "NULL";
+						break;
+					default:
+						break;
+					}
+				} catch (NullPointerException e) {
+					// do something clever with the exception
+					System.out.println("nullException" + e.getMessage());
+				}
+			dataMap.put(headerKeys.get(cellNum), cellObjVal);
+		}
+		return dataMap;
+	}
+}
